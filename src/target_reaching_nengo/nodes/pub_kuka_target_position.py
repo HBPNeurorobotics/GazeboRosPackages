b1029b536cb05747902faef4fb772a09c9f57212
Daniel Azanov <azanov@fzi.de>
2020-03-19 19:46:16 +0100

    use last pred pos for kuka target position

--- src/target_reaching_nengo/nodes/pub_kuka_target_position.py
+++ src/target_reaching_nengo/nodes/pub_kuka_target_position.py
@@ -23,11 +23,16 @@ class PubKukaTarget:
         self.pred_pos_frame = rospy.get_param('~pred_pos_frame', 'world')
         self.gazebo_pos_frame = rospy.get_param('~gazebo_pos_frame', 'world')
 
-        self.gazebo_target_link_name = rospy.get_param('~gazebo_target_link_name', 'ball::ball')
-        self.get_model_state = rospy.Subscriber('/gazebo/link_states', LinkStates, self.gazebo_link_states_cb, queue_size=1)
+        #self.gazebo_target_link_name = rospy.get_param('~gazebo_target_link_name', 'ball::ball')
+        #self.get_model_state = rospy.Subscriber('/gazebo/link_states', LinkStates, self.gazebo_link_states_cb, queue_size=1)
 
-        #pred_pos_topic = rospy.get_param('~pred_pos_topic', '/pred_pos')
-        #self.pred_pos_sub = rospy.Subscriber(pred_pos_topic, Float32MultiArray, self.pred_pos_cb, queue_size=1)
+        pred_pos_topic = rospy.get_param('~pred_pos_topic', '/pred_pos')
+        self.pred_pos_sub = rospy.Subscriber(pred_pos_topic, Float32MultiArray, self.pred_pos_cb, queue_size=1)
+        self.wait_for_next_pred = False
+
+        self.error = None
+        error_topic = rospy.get_param('~error_topic', '/error')
+        self.error_sub = rospy.Subscriber(error_topic, Float64MultiArray, self.error_cb, queue_size=1)
 
         target_position_topic = rospy.get_param('~target_position_topic', '/target_position')
         self.target_position_pub = rospy.Publisher(target_position_topic, PointStamped, queue_size=1)
@@ -44,9 +49,18 @@ class PubKukaTarget:
         if gazebo_target_position is not None:
             self.target_position = to_point_stamped(self.gazebo_pos_frame, gazebo_target_position)
 
+    def error_cb(self, data):
+        self.error = data.data
+
     def pred_pos_cb(self, data):
-        if len(data.data) < 3:
+        has_nan = all([e == e for e in data.data])
+        if has_nan
+            if self.wait_for_next_pred:
+                self.wait_for_next_pred = False
             return
+        if self.wait_for_next_pred:
+            return
+        self.wait_for_next_pred = True
         pred_pos = data.data[-3:]
         self.target_position = to_point_stamped(self.pred_pos_frame, pred_pos)
 
